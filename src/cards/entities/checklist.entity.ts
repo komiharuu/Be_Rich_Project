4c9e97720ba4fbd6d4f423a7ffb59933cfac386b
komiharuu <biomilky3@gmail.com>
2024-07-12 12:22:10 +0900

    댓글, 체크리스트 엔티티 추가

---
+++ src/cards/entities/checklist.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Card } from './card.entity';
+
+@Entity('comments')
+export class Checklist {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+
+  @Column({ unsigned: true })
+  card_id: number;
+
+  @Column({ type: 'varchar' })
+  title: string;
+
+  @Column({ type: 'varchar' })
+  item: string;
+
+  @Column({ type: 'boolean', default: false })
+  is_completed: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @ManyToOne((type): typeof Card => Card, (card): Checklist[] => card.checklists, {
+    onDelete: 'CASCADE',
+  })
+  card: Card;
+}
