bce728bceb3610781187b19e2aae705dc5efa252
AYAAKIASA <ayaakiasa23@gmail.com>
2024-07-12 19:53:44 +0900

    Add/user dto

---
+++ src/users/dto/user-response.dto.ts
@@ -0,0 +1,9 @@
+export class UserResponseDto {
+    id!: number;
+    email!: string;
+    nickname!: string;
+    profileImg?: string;
+    isDeleted!: boolean;
+    createdAt!: Date;
+    updatedAt!: Date;
+  }
