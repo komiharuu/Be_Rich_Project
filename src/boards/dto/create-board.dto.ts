b5d58556d08a0fa89f3a7c679a400b65714fb0c1
soobeen0301 <xg12041@gmail.com>
2024-07-12 15:37:00 +0900

    add/create-board dto

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,1 +1,13 @@
-export class CreateBoardDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateBoardDto {
+  @IsString()
+  @IsNotEmpty({ message: '보드 제목을 입력해주세요.' })
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  backgroundColor: string;
+}
