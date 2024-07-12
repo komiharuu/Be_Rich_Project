d95e1d129153a1d7a39442ddf367a9a40b928f9c
AYAAKIASA <ayaakiasa23@gmail.com>
2024-07-12 19:42:21 +0900

    Add/user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  nickname!: string;
+
+  @Column()
+  password!: string;
+
+  @Column({ name: 'profile_img', nullable: true })
+  profileImg!: string;
+
+  @Column({ name: 'refresh_token', nullable: true })
+  refreshToken!: string;
+
+  @Column({ name: 'is_deleted', default: false })
+  isDeleted!: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+}
