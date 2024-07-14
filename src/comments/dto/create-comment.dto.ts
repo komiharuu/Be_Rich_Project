e36e059684b31818018a703bc82c46f95f03a6f8
komiharuu <biomilky3@gmail.com>
2024-07-14 16:18:07 +0900

    댓글 dto 초안

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,13 @@
-export class CreateCommentDto {}
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateCommentDto {
+  @Type(() => Number)
+  @IsNumber()
+  @IsNotEmpty({ message: '카드 id를 입력해주세요.' })
+  card_id: number;
+
+  @IsString()
+  @IsNotEmpty({ message: '내용을 입력해주세요.' })
+  comment: string;
+}
