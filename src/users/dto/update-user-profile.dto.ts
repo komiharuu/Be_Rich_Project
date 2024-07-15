05309466dcd37171a8479512a08a46716762bd3a
jkc-mycode <jkc9709@naver.com>
2024-07-15 23:57:30 +0900

    Add/Auth, User overall code

---
+++ src/users/dto/update-user-profile.dto.ts
@@ -0,0 +1,19 @@
+import { Optional } from '@nestjs/common';
+import { IsString, ValidateIf } from 'class-validator';
+
+export class UpdateUserProfileDto {
+  @Optional()
+  @ValidateIf((_, value) => value !== undefined)
+  @IsString()
+  email: string;
+
+  @Optional()
+  @ValidateIf((_, value) => value !== undefined)
+  @IsString()
+  nickname: string;
+
+  @Optional()
+  @ValidateIf((_, value) => value !== undefined)
+  @IsString()
+  profileImg: string;
+}
--- src/users/dto/user-response.dto.ts
+++
@@ -1,9 +0,0 @@
-export class UserResponseDto {
-    id: number;
-    email: string;
-    nickname: string;
-    profileImg: string;
-    isDeleted: boolean;
-    createdAt: Date;
-    updatedAt: Date;
-  }
