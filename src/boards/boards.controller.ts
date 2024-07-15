b8caf246347b2d0cbddd7ff295137aa26b4ebbec
soobeen0301 <xg12041@gmail.com>
2024-07-15 14:56:55 +0900

    Refactor/board crud code update, board guard add

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -17,7 +17,8 @@ import { UpdateBoardDto } from './dto/update-board.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/users/entities/user.entity';
-import { AuthUser } from 'src/auth/user.decorator';
+import { BoardMemberGuard } from './board-member.guard';
+import { BoardOwnerGuard } from './board-owner.guard';
 
 @ApiTags('보드')
 @Controller('boards')
@@ -30,48 +31,56 @@ export class BoardsController {
   @UseGuards(AuthGuard('jwt'))
   @HttpCode(HttpStatus.CREATED)
   @Post()
-  async createBoard(@Body() createBoardDto: CreateBoardDto, @Req() req) {
-    const userId = req.user.id;
-    return await this.boardsService.createBoard(createBoardDto, userId);
+  async createBoard(@Body() createBoardDto: CreateBoardDto, @Req() req: { user: User }) {
+    const user = req.user;
+    return await this.boardsService.createBoard(createBoardDto, user.id);
   }
 
   /**
    * 보드 목록 조회
    */
+  @UseGuards(AuthGuard('jwt'), BoardMemberGuard)
   @HttpCode(HttpStatus.OK)
   @Get()
-  getBoardList(@AuthUser() user: User) {
+  getBoardList(@Req() req: { user: User }) {
+    const user = req.user;
     return this.boardsService.getBoardList(user);
   }
 
   /**
    * 보드 상세 조회
    */
+  @UseGuards(AuthGuard('jwt'), BoardMemberGuard)
   @HttpCode(HttpStatus.OK)
   @Get(':boardId')
-  getBoardDetail(@Param('boardId') id: number, @AuthUser() user: User) {
+  getBoardDetail(@Param('boardId') id: number, @Req() req: { user: User }) {
+    const user = req.user;
     return this.boardsService.getBoardDetail(+id, user);
   }
 
   /**
    * 보드 수정
    */
+  @UseGuards(AuthGuard('jwt'), BoardOwnerGuard)
   @HttpCode(HttpStatus.OK)
   @Patch(':boardId')
   updateBoard(
     @Param('boardId') id: number,
     @Body() updateBoardDto: UpdateBoardDto,
-    @AuthUser() user: User
+    @Req() req: { user: User }
   ) {
+    const user = req.user;
     return this.boardsService.updateBoard(+id, updateBoardDto, user);
   }
 
   /**
    * 보드 삭제
    */
+  @UseGuards(AuthGuard('jwt'), BoardOwnerGuard)
   @HttpCode(HttpStatus.OK)
   @Delete(':boardId')
-  deleteBoard(@Param('boardId') id: number, @AuthUser() user: User) {
+  deleteBoard(@Param('boardId') id: number, @Req() req: { user: User }) {
+    const user = req.user;
     return this.boardsService.deleteBoard(+id, user);
   }
 }
