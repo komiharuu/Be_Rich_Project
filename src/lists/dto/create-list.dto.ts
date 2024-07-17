fec36816b131337912a11d48feb6d93e3bbc7c80
Parkseojin2 <bym401543@gmail.com>
2024-07-17 20:31:51 +0900

    이것저것 수정

--- src/lists/dto/create-list.dto.ts
+++ src/lists/dto/create-list.dto.ts
@@ -1,11 +1,8 @@
-import { IsNotEmpty, IsString, IsInt, IsBoolean, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsString, IsInt } from 'class-validator';
 
 export class CreateListDto {
   @IsString()
   @IsNotEmpty({ message: '리스트 제목을 입력해주세요.' })
   title: string;
 
-  @IsInt()
-  @IsNotEmpty({ message: '보드 ID를 입력해주세요.' })
-  boardId: number;
 }
